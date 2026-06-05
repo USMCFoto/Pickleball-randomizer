2a648f953abc018657041a2294cc4ba63a157fa0
USMCFoto <usmcfoto@gmail.com>
2026-06-04 23:06:10 -0700

    Update 1.5 app.py
    Really 2.0 but adding text version to title.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import random
 
 st.set_page_config(
-    page_title="Pickleball Randomizer",
+    page_title="Pickleball Randomizer v1.5",
     page_icon="🥒",
     layout="centered",
     initial_sidebar_state="collapsed"
